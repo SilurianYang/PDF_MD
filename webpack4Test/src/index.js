be2767ba3fa1bcce3bb1af680b386076ee480669
SilurianYang <1606726660@qq.com>
2020-04-12 21:49:09 +0800

    test project corresponding to the document

---
+++ webpack4Test/src/index.js
@@ -0,0 +1,10 @@
+import './css/index.css';
+import './css/index.scss';
+import './fonts/iconfont.css';
+
+function add(x, y) {
+  return x + y;
+}
+
+// eslint-disable-next-line
+console.log(add(5, 2));
